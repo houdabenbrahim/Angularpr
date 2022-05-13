0be1a6d4393f58ad88713ef605023d7376039e4c
houdaben <git@gitlab.com:houdabenbrahim98/artik-consulting.git>
2022-05-13 14:18:17 +0200

    SPA and routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ArtikComponent } from './artik/artik.component';
 import { ArtikListComponent } from './artik-list/artik-list.component';
 import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 @NgModule({
@@ -15,14 +15,13 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     ArtikComponent,
     ArtikListComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingPageComponent
   ],
 
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
 
   providers: [],
